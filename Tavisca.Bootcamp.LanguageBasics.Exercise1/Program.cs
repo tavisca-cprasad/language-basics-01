9c7ac97268375b42fb77b342cfe73ee8aa038f02
Chandan-97 <cp564738@gmail.com>
2019-06-24 14:16:45 +0530

    Excercise1 Completed

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,6 +23,45 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
+            int idx1 = equation.IndexOf("*");
+            int idx2 = equation.IndexOf("=");
+
+            String s1 = equation.Substring(0, idx1);
+            String s2 = equation.Substring(idx1+1, idx2-idx1-1);
+            String s3 = equation.Substring(idx2+1);
+
+            if(s1.IndexOf("?") != -1 || s2.IndexOf("?") != -1){
+                if(s2.IndexOf("?") != -1){
+                    String temp = s1;
+                    s1 = s2;
+                    s2 = temp;
+                }
+                
+                int val3 = int.Parse(s3);
+                int val2 = int.Parse(s2);
+
+                int val1 = val3 / val2;
+                String s1original = val1.ToString();
+                if(s1.Length != s1original.Length) return -1;
+
+                int idx = s1.IndexOf("?");
+                char ch = s1original[idx];
+
+                return ch - '0' == 0 && idx == 0 ? -1 : (ch-'0');
+            }
+            else{
+                int val1 = int.Parse(s1);
+                int val2 = int.Parse(s2);
+                int val3 = val1 * val2;
+
+                String s3original = val3.ToString();
+                if(s3.Length != s3original.Length) return -1;
+
+                int idx = s3.IndexOf("?");
+                char ch = s3original[idx];
+
+                return ch - '0' == 0 && idx == 0 ? -1 : (ch - '0');
+            }
             throw new NotImplementedException();
         }
     }
